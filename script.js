f8b39857a75163740c977d36f689734570fe8fc6
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-09-19 17:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
-
+let ol = document.getElementById("infi-list");
+let item = 1;
+function load()
+	{
+		for(let i=1;i<=20;i++)
+			{
+				let li = document.createElement("li");
+				li.innerText = `item ${item}`;
+				item++;
+				ol.appendChild(li);
+			}
+	}
+ol.addEventListener("scroll", () => {
+	if(ol.scrollTop + ol.clientHeight >= ol.scrollHeight)
+	{
+		load();
+	}
+})
+load();
